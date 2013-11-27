9830acf768e374caa77fc8c3c088d916a50205e5
Kepler Sticka-Jones <kbsj269@gmail.com>
2013-11-27 12:55:32 -0700

    Fixed Logger.

--- src/main/java/k2b6s9j/cloaked_batman/cloakedBatman.java
+++ src/main/java/k2b6s9j/cloaked_batman/cloakedBatman.java
@@ -1,5 +1,6 @@
 package k2b6s9j.cloaked_batman;
 
+import cpw.mods.fml.common.FMLLog;
 import cpw.mods.fml.common.Loader;
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.Mod.EventHandler;
@@ -8,15 +9,13 @@ import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.event.FMLPostInitializationEvent;
 import cpw.mods.fml.common.event.FMLServerStartingEvent;
 import k2b6s9j.cloaked_batman.util.chickenbones.DepLoader;
+import k2b6s9j.cloaked_batman.util.log.cloakedLogger;
 import org.mcstats.MetricsLite;
 
 import java.util.logging.Logger;
 
 @Mod(modid = "cloaked-batman", name = "cloaked batman", version = "1.0.1", dependencies="after:CodeChickenCore;")
 public class cloakedBatman {
-
-    //Logger
-    public static final Logger logger = Logger.getLogger("cloaked batman");
 	
 	@EventHandler
 	public void serverStarting(FMLServerStartingEvent event) {
@@ -27,9 +26,9 @@ public class cloakedBatman {
 	
 	@EventHandler
 	public void Init(FMLInitializationEvent event) {
-        logger.info("cloaked batman");
-        logger.info("Copyright Kepler Sticka-Jones 2013");
-        logger.info("http://k2b6s9j.com/projects/minecraft/cloaked-batman");
+        cloakedLogger.info("cloaked batman");
+        cloakedLogger.info("Copyright Kepler Sticka-Jones 2013");
+        cloakedLogger.info("http://k2b6s9j.com/projects/minecraft/cloaked-batman");
 	}
 	
 	@EventHandler
@@ -38,9 +37,9 @@ public class cloakedBatman {
 			try {
 			    MetricsLite metrics = new MetricsLite(mod.getName(), mod.getVersion());
 			    metrics.start();
-			    logger.info("Sent statistical information for "+mod.getModId ()+"("+ mod.getName()+")"+" version "+mod.getVersion()+" to MCStats.");
+                cloakedLogger.info("Sent statistical information for "+mod.getName()+" ("+ mod.getModId()+")"+" version "+mod.getVersion()+" to MCStats.");
 			} catch (Exception e) {
-			    logger.warning(mod.getModId ()+"("+mod.getName()+") failed to submit statistical information to MCStats");
+                cloakedLogger.warning(mod.getName()+" ("+mod.getModId()+") failed to submit statistical information to MCStats");
 			}
 		}
 	}
---
+++ src/main/java/k2b6s9j/cloaked_batman/util/log/cloakedLogger.java
@@ -0,0 +1,30 @@
+package k2b6s9j.cloaked_batman.util.log;
+
+import cpw.mods.fml.common.FMLLog;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class cloakedLogger {
+    private static Logger log = Logger.getLogger("cloaked batman");
+
+    public static void info(String msg) {
+        log.log(Level.INFO, msg);
+    }
+
+    public static void warning(String msg) {
+        log.log(Level.WARNING, msg);
+    }
+
+    public static void severe(String msg) {
+        log.log(Level.SEVERE, msg);
+    }
+
+    public static Logger getLogger() {
+        return log;
+    }
+
+    static {
+        log.setParent(FMLLog.getLogger());
+    }
+}

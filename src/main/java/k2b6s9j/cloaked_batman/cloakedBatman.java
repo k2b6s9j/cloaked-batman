e0d64e52ada2372b2d7b21de315c3106dc44a850
Kepler Sticka-Jones <kbsj269@gmail.com>
2013-11-27 00:11:37 -0700

    Only use my horrible dependancy loader if the installation doesn’t already have CCC.

--- src/main/java/k2b6s9j/cloaked_batman/cloakedBatman.java
+++ src/main/java/k2b6s9j/cloaked_batman/cloakedBatman.java
@@ -6,21 +6,23 @@ import cpw.mods.fml.common.Mod.EventHandler;
 import cpw.mods.fml.common.ModContainer;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.event.FMLPostInitializationEvent;
-import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+import cpw.mods.fml.common.event.FMLServerStartingEvent;
 import k2b6s9j.cloaked_batman.util.chickenbones.DepLoader;
 import org.mcstats.MetricsLite;
 
 import java.util.logging.Logger;
 
-@Mod(modid = "cloaked-batman", name = "cloaked batman", version = "1.0.1")
+@Mod(modid = "cloaked-batman", name = "cloaked batman", version = "1.0.1", dependencies="after:CodeChickenCore;")
 public class cloakedBatman {
 
     //Logger
     public static final Logger logger = Logger.getLogger("cloaked batman");
 	
 	@EventHandler
-	public void preInit(FMLPreInitializationEvent event) {
-        DepLoader.load();
+	public void serverStarting(FMLServerStartingEvent event) {
+        if (!Loader.isModLoaded("CodeChickenCore")) {
+            DepLoader.load();
+        }
 	}
 	
 	@EventHandler

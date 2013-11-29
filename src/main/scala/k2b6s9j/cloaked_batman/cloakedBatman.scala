6db823bc35ef92608edcb30542ac660fb08160ac
Kepler Sticka-Jones <kbsj269@gmail.com>
2013-11-29 00:27:45 -0700

    Began to fix Scala compilation.

--- src/main/scala/k2b6s9j/cloaked_batman/cloakedBatman.scala
+++ src/main/scala/k2b6s9j/cloaked_batman/cloakedBatman.scala
@@ -8,9 +8,10 @@ import cpw.mods.fml.common.event.FMLPostInitializationEvent
 import cpw.mods.fml.common.event.FMLServerStartingEvent
 import k2b6s9j.cloaked_batman.util.chickenbones.DepLoader
 import org.mcstats.Metrics
+import org.mcstats.Metrics.{Graph}
 import k2b6s9j.cloaked_batman.util.log.ModLogger
 import java.io.IOException
-import com.sun.corba.se.impl.orbutil.graph.Graph
+import scala.collection.JavaConversions._
 
 @Mod(modid = "cloaked-batman", name = cloakedBatman.name, version = cloakedBatman.version, modLanguage = "scala", dependencies="after:CodeChickenCore;")
 object cloakedBatman {
@@ -34,24 +35,24 @@ object cloakedBatman {
 
 	@EventHandler
 	def postInit(event: FMLPostInitializationEvent) {
-		submitIndividualMods()
-    addLoadedMods()
+    val modList = Loader.instance().getActiveModList
+    modList.toList.foreach(submitIndividualMod)
+    modList.toList.foreach(addLoadedMod)
 	}
 
   /*
     This method finds all of the mods currently loaded in game, their names, IDs, and versions and submits them as individual plugins on MCStats.
     @author Kepler (k2b6s9j) B.I. Sticka-Jones
    */
-  def submitIndividualMods() {
-    for (mod: ModContainer <- Loader.instance().getActiveModList) {
-      try
-        Metrics.metrics = new Metrics(mod.getName, mod.getVersion)
-        metrics.start()
-        ModLogger.info("Sent statistical information for " + mod.getName + " (" + mod.getModId + ")" + " version " + mod.getVersion + " to MCStats.")
-      catch {
-        case e: Exception => ModLogger.warning(mod.getName + " (" + mod.getModId + ") failed to submit statistical information to MCStats")
-        case e: Exception => e.printStackTrace()
-      }
+  def submitIndividualMod(mod: ModContainer): Unit = {
+    try {
+      val metrics: Metrics = new Metrics(mod.getName, mod.getVersion)
+      metrics.start()
+      ModLogger.info("Sent statistical information for " + mod.getName + " (" + mod.getModId + ")" + " version " + mod.getVersion + " to MCStats.")
+    }
+    catch {
+      case e: Exception => ModLogger.warning(mod.getName + " (" + mod.getModId + ") failed to submit statistical information to MCStats")
+      case e: Exception => e.printStackTrace()
     }
   }
 
@@ -59,19 +60,18 @@ object cloakedBatman {
     This method finds all of the mods currently loaded in game, their name, IDs, and version and adds them to a graph on the cloaked batman page on MCStats.
     @author Kepler (k2b6s9j) B.I. Sticka-Jones
    */
-  def addLoadedMods() {
-    for (mod: ModContainer <- Loader.instance().getActiveModList) {
-      try
-        def metrics: Metrics = new Metrics(name, version)
+  def addLoadedMod(mod: ModContainer): Unit = {
+    try {
+      def metrics: Metrics = new Metrics(name, version)
 
-        val modsLoaded: Graph = metrics.createGraph("Mods Loaded")
+      val modsLoaded: Graph = metrics.createGraph("Mods Loaded")
 
-        modsLoaded.addPlotter(Metrics.Plotter("%s (%s) version %s", mod.getName, mod.getModId, mod.getVersion))
+      modsLoaded.addPlotter(Metrics.Plotter("%s %s", mod.getName, mod.getVersion))
 
-        metrics.start()
-      catch{
-        case e: IOException => ModLogger.warning(e.getMessage)
-      }
+      metrics.start()
+    }
+    catch{
+      case e: IOException => ModLogger.warning(e.getMessage)
     }
   }
 }

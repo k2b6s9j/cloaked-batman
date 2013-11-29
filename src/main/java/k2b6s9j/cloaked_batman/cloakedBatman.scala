9d49a52492e9d2f9b2cf0f6fcc9ba64fa214e07f
Kepler Sticka-Jones <kbsj269@gmail.com>
2013-11-28 17:09:34 -0700

    Ported completely to scala

--- src/main/java/k2b6s9j/cloaked_batman/cloakedBatman.scala
+++ src/main/java/k2b6s9j/cloaked_batman/cloakedBatman.scala
@@ -1,46 +1,43 @@
-package k2b6s9j.cloaked_batman;
+package k2b6s9j.cloaked_batman
 
-import cpw.mods.fml.common.FMLLog;
-import cpw.mods.fml.common.Loader;
-import cpw.mods.fml.common.Mod;
-import cpw.mods.fml.common.Mod.EventHandler;
-import cpw.mods.fml.common.ModContainer;
-import cpw.mods.fml.common.event.FMLInitializationEvent;
-import cpw.mods.fml.common.event.FMLPostInitializationEvent;
-import cpw.mods.fml.common.event.FMLServerStartingEvent;
-import k2b6s9j.cloaked_batman.util.chickenbones.DepLoader;
-import k2b6s9j.cloaked_batman.util.log.cloakedLogger;
-import org.mcstats.MetricsLite;
+import cpw.mods.fml.common.{Mod, Loader, ModContainer}
 
-import java.util.logging.Logger;
+import cpw.mods.fml.common.Mod.EventHandler
+import cpw.mods.fml.common.event.FMLInitializationEvent
+import cpw.mods.fml.common.event.FMLPostInitializationEvent
+import cpw.mods.fml.common.event.FMLServerStartingEvent
+import k2b6s9j.cloaked_batman.util.chickenbones.DepLoader
+import k2b6s9j.cloaked_batman.util.log.cloakedLogger
+import org.mcstats.MetricsLite
 
-@Mod(modid = "cloaked-batman", name = "cloaked batman", version = "1.0.2", dependencies="after:CodeChickenCore;")
-public class cloakedBatman {
+@Mod(modid = "cloaked-batman", name = "cloaked batman", version = "1.0.2", modLanguage = "scala", dependencies="after:CodeChickenCore;")
+object cloakedBatman {
 	
 	@EventHandler
-	public void serverStarting(FMLServerStartingEvent event) {
+	def serverStarting(event: FMLServerStartingEvent) {
         if (!Loader.isModLoaded("CodeChickenCore")) {
-            DepLoader.load();
+            DepLoader.load()
         }
 	}
 	
 	@EventHandler
-	public void Init(FMLInitializationEvent event) {
-        cloakedLogger.info("cloaked batman");
-        cloakedLogger.info("Copyright Kepler Sticka-Jones 2013");
-        cloakedLogger.info("http://k2b6s9j.com/projects/minecraft/cloaked-batman");
-	}
+	def Init(event: FMLInitializationEvent) {
+    cloakedLogger.info("cloaked batman")
+    cloakedLogger.info("Copyright Kepler Sticka-Jones 2013")
+    cloakedLogger.info("http://k2b6s9j.com/projects/minecraft/cloaked-batman")
+  }
 	
 	@EventHandler
-	public void postInit(FMLPostInitializationEvent event) throws Exception {
-		for (ModContainer mod : Loader.instance().getModList()) {
-			try {
-			    MetricsLite metrics = new MetricsLite(mod.getName(), mod.getVersion());
-			    metrics.start();
-                cloakedLogger.info("Sent statistical information for "+mod.getName()+" ("+ mod.getModId()+")"+" version "+mod.getVersion()+" to MCStats.");
-			} catch (Exception e) {
-                cloakedLogger.warning(mod.getName()+" ("+mod.getModId()+") failed to submit statistical information to MCStats");
-			}
+	def postInit(event: FMLPostInitializationEvent) {
+		for (mod: ModContainer <- Loader.instance().getModList) {
+      try
+        MetricsLite.metrics = new MetricsLite(mod.getName, mod.getVersion)
+        metrics.start()
+        cloakedLogger.info("Sent statistical information for " + mod.getName + " (" + mod.getModId + ")" + " version " + mod.getVersion + " to MCStats.")
+      catch {
+        case e: Exception => cloakedLogger.warning(mod.getName + " (" + mod.getModId + ") failed to submit statistical information to MCStats")
+        case e: Exception => e.printStackTrace()
+      }
 		}
 	}
 

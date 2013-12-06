4514e60fa276a44453b33f8ae4046ef11a3fe324
Kepler Sticka-Jones <kbsj269@gmail.com>
2013-12-06 16:57:45 -0700

    fixed test scala compilation

--- src/test/scala/k2b6s9j/cloaked_batman/report/AddLoadedModTest.java
+++ src/test/scala/k2b6s9j/cloaked_batman/report/AddLoadedModTest.java
@@ -2,7 +2,7 @@ package k2b6s9j.cloaked_batman.report;
 
 import org.testng.annotations.Test;
 
-public class AddLoadedMod {
+public class AddLoadedModTest {
     @Test
     public void testAddLoadedMod() throws Exception {
 

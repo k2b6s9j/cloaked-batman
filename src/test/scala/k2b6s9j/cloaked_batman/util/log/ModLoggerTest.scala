836193bb511f71d8ea5dbec9fca4022129922422
Kepler Sticka-Jones <kbsj269@gmail.com>
2013-12-07 13:00:21 -0700

    grammer

--- src/test/scala/k2b6s9j/cloaked_batman/util/log/ModLoggerTest.scala
+++ src/test/scala/k2b6s9j/cloaked_batman/util/log/ModLoggerTest.scala
@@ -10,27 +10,27 @@ class ModLoggerTest extends FlatSpec with Matchers {
   
   var msg: String = "Test Log Message"
   
-  "A log message" should "log an information message if given an information message" in {
+  "An information log message" should "be logged if given an message" in {
      
   }
   
-  it should "return a NullPointerException if no information message is given" in {
+  it should "return a NullPointerException if no message is given" in {
     
   } 
   
-  it should "log a warning message if given a warning message" in {
+  "A warning message" should "be logged if given a message" in {
     
   }
   
-  it should "return a NullPointerException if no warning message is given" in {
+  it should "return a NullPointerException if no message is given" in {
     
   }
   
-  it should "log a severe message if given a severe message" in {
+  "A severe message" should "be logged if given a message" in {
     
   }
   
-  it should "return a NullPointerException if no severe message is given" in {
+  it should "return a NullPointerException if no message is given" in {
     
   }
   

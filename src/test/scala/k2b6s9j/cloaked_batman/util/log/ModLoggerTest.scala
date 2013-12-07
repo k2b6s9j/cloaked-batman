a9e7a110be29af2154f69456c7f1266ef1e5837b
Kepler Sticka-Jones <kbsj269@gmail.com>
2013-12-07 14:12:20 -0700

    Just be succesful damn it

--- src/test/scala/k2b6s9j/cloaked_batman/util/log/ModLoggerTest.scala
+++ src/test/scala/k2b6s9j/cloaked_batman/util/log/ModLoggerTest.scala
@@ -4,38 +4,28 @@ import org.junit.Assert
 import org.junit.runner.RunWith
 import org.scalatest._
 import org.scalatest.junit.JUnitRunner
+import java.util.logging.Logger
 
 @RunWith(classOf[JUnitRunner])
-class ModLoggerTest extends FlatSpec with Matchers {
+class ModLoggerTest extends FlatSpec with Matchers with BeforeAndAfter {
   
-  var msg: String = "Test Log Message"
+   var msg: String = "Test Log Message"
+   //var logger =  new ModLogger() 
   
   "An information log message" should "be logged if given an message" in {
-     
+	//ModLogger.info(msg) 
   }
-  
-  it should "return a NullPointerException if no message is given" in {
-    
-  } 
   
   "A warning message" should "be logged if given a message" in {
-    
-  }
-  
-  it should "return a NullPointerException if no message is given" in {
-    
+    //ModLogger.warning(msg)
   }
   
   "A severe message" should "be logged if given a message" in {
-    
-  }
-  
-  it should "return a NullPointerException if no message is given" in {
-    
+    //ModLogger.severe(msg)
   }
   
   "The logger" should "return itself if getLogger is called" in {
-    
+    //logger.getLogger shouldBe ModLogger
   }
   
 }

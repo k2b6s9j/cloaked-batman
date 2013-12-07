84ab60e67b6145f8eadca1c138a60e20de0f5a27
Kepler Sticka-Jones <kbsj269@gmail.com>
2013-12-07 11:58:02 -0700

    no seriously

--- src/test/scala/k2b6s9j/cloaked_batman/util/log/ModLoggerTest.scala
+++ src/test/scala/k2b6s9j/cloaked_batman/util/log/ModLoggerTest.scala
@@ -3,27 +3,24 @@ package k2b6s9j.cloaked_batman.util.log
 import org.junit.Test
 import org.junit.Assert
 import org.junit.runner.RunWith
+import org.scalatest.junit.JUnitRunner
 
 @RunWith(classOf[JUnitRunner])
 object ModLoggerTest {
   var msg: String = "Test Message"
   
-  @Test
   def testInfo() {
     Assert.assertNotNull(ModLogger.info(msg))
   }
   
-  @Test
   def testWarning() {
     Assert.assertNotNull(ModLogger.warning(msg))
   }
   
-  @Test
   def testSevere() {
     Assert.assertNotNull(ModLogger.severe(msg))
   }
   
-  @Test
   def testGetLogger() {
     
   }

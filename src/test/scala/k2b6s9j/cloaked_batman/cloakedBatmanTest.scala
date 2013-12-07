6d41bf64456a7f2b35211d9db591543437f22e07
Kepler Sticka-Jones <kbsj269@gmail.com>
2013-12-06 22:18:41 -0700

    HANDSSSSSSSSSSSS!!!! I AM FUCKING TYPING!!!!!!!!!

---
+++ src/test/scala/k2b6s9j/cloaked_batman/cloakedBatmanTest.scala
@@ -0,0 +1,31 @@
+package k2b6s9j.cloaked_batman
+
+import cpw.mods.fml.common.event.FMLInitializationEvent
+import cpw.mods.fml.common.event.FMLPostInitializationEvent
+import cpw.mods.fml.common.ModContainer
+
+import org.junit.Assert.assertEquals
+import org.junit.Test
+import org.junit.Ignore
+import org.junit.runner.RunWith
+import org.junit.runners.JUnit4
+
+@RunWith(classOf[JUnit4])
+object cloakedBatmanTest {
+  
+  @Test
+  def Init(event: FMLInitializationEvent) {
+    
+  }
+  
+  @Test
+  def postInit(event: FMLPostInitializationEvent) {
+    
+  }
+  
+  @Test
+  def submitIndividualMod(mod: ModContainer): Unit = {
+    
+  }
+
+}

5919de1d1e7be96a88b44a7676385b41b59f858a
Kepler Sticka-Jones <kbsj269@gmail.com>
2013-12-06 23:08:55 -0700

    whatever.

--- src/test/scala/k2b6s9j/cloaked_batman/cloakedBatmanTest.scala
+++ src/test/scala/k2b6s9j/cloaked_batman/cloakedBatmanTest.scala
@@ -3,28 +3,24 @@ package k2b6s9j.cloaked_batman
 import cpw.mods.fml.common.event.FMLInitializationEvent
 import cpw.mods.fml.common.event.FMLPostInitializationEvent
 import cpw.mods.fml.common.ModContainer
-
-import org.junit.Assert.assertEquals
 import org.junit.Test
-import org.junit.Ignore
-import org.junit.runner.RunWith
-import org.junit.runners.JUnit4
+import cpw.mods.fml.common.Mod
 
-@RunWith(classOf[JUnit4])
-object cloakedBatmanTest {
+@Mod(modid = "cloaked-batman")
+class cloakedBatmanTest {
   
   @Test
-  def Init(event: FMLInitializationEvent) {
+  def Init() {
     
   }
   
   @Test
-  def postInit(event: FMLPostInitializationEvent) {
+  def postInit() {
     
   }
   
   @Test
-  def submitIndividualMod(mod: ModContainer): Unit = {
+  def submitIndividualMod() {
     
   }
 

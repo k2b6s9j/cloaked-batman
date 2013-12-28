45715ebf85fd0b882f4c262ff1d6538423b14272
Kepler Sticka-Jones <kbsj269@gmail.com>
2013-12-28 13:23:18 -0700

    Extend FMLLog

--- src/main/scala/k2b6s9j/cloaked_batman/util/log/ModLogger.scala
+++ src/main/scala/k2b6s9j/cloaked_batman/util/log/ModLogger.scala
@@ -5,23 +5,7 @@ import cpw.mods.fml.common.FMLLog
 import org.apache.logging.log4j.Level;
 import org.apache.logging.log4j.Logger;
 
-object ModLogger {
+object ModLogger extends FMLLog {
     def log: Logger = Logger.getLogger("cloaked batman")
     log.setParent(FMLLog.getLogger)
-
-    def info(msg: String) {
-        log.log(Level.INFO, msg)
-    }
-
-    def warning(msg: String) {
-        log.log(Level.WARNING, msg)
-    }
-
-    def severe(msg: String) {
-        log.log(Level.SEVERE, msg)
-    }
-
-    def getLogger(): Logger = {
-        log: Logger
-    }
 }

b9efe3346c06fdb9eaf45d166962e00df28f41b5
Kepler Sticka-Jones <kbsj269@gmail.com>
2013-12-28 13:50:08 -0700

    Revert "Extend FMLLog"
    This reverts commit 45715ebf85fd0b882f4c262ff1d6538423b14272.

--- src/main/scala/k2b6s9j/cloaked_batman/util/log/ModLogger.scala
+++ src/main/scala/k2b6s9j/cloaked_batman/util/log/ModLogger.scala
@@ -5,7 +5,23 @@ import cpw.mods.fml.common.FMLLog
 import org.apache.logging.log4j.Level;
 import org.apache.logging.log4j.Logger;
 
-object ModLogger extends FMLLog {
+object ModLogger {
     def log: Logger = Logger.getLogger("cloaked batman")
     log.setParent(FMLLog.getLogger)
+
+    def info(msg: String) {
+        log.log(Level.INFO, msg)
+    }
+
+    def warning(msg: String) {
+        log.log(Level.WARNING, msg)
+    }
+
+    def severe(msg: String) {
+        log.log(Level.SEVERE, msg)
+    }
+
+    def getLogger(): Logger = {
+        log: Logger
+    }
 }
